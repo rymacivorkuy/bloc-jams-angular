f005eca4c830da74ccfda7b1b1279eec45722bdc
Ryan Kuykendall <rymacivorkuy@gmail.com>
2017-05-15 10:16:21 -0500

    Completed assignment 5

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,8 +14,9 @@
             })
             .state('album', {
                 url: '/album',
+                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
-            });
+            })
             .state('collection', {
                 url: '/collection',
                 controller: 'CollectionCtrl as collection',
